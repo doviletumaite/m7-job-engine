bf5a5099e3a9bdec5f01b04dba77d7a547e65bd8
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-10-25 14:56:04 +0200

    Update Favourites.jsx

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -16,7 +16,7 @@ class Favourites extends React.Component {
                     <Col xs={12}>
                         <ListGroup>
                         {
-                            this.props.favourites.map(f => (
+                            this.props.favourites.elements.map(f => (
                                 <ListGroupItem>
                                     <StarFill onClick={() => this.props.removeFromFav(f)} />
                                     <span>{ f }</span>
